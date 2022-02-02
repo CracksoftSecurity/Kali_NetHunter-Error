99072b33e9f6931188d5ec84fd46f7bef4671e6e
HACKER-CRACKSOFT <69074572+CrackSoft900@users.noreply.github.com>
2022-02-02 21:51:47 +0530

    Update and rename run.py.txt to run.py

--- run.py
+++ run.py
@@ -3,5 +3,3 @@ a=input("Enter Your Name \n >>")
 print()
 print("Dear "+a+" Steps to Fix Error \n 1st- Uninstall the Termux \n 2nd- Install the Termux from Playstore \n 3rd- Please Don't install Nethunter Again or \n Cheack your Device in KaliNetHunter, List link is >>bit.ly/3GowBrv")
 print("If your Device is not in list then don't install Nehunter Again , becaure it'll not install and only give you Error \n\n\n\nThanks Dear "+a+" for using CRACKSOFT SECURITY INDIA 1st Tool")
-
-
