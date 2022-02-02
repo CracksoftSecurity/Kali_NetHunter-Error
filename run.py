fd1f37e2481b92e97c7e01e9781ec30ab07c8ea2
HACKER-CRACKSOFT <69074572+CrackSoft900@users.noreply.github.com>
2022-02-02 22:04:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
+print("  ")
+print("   _____             __             _____    ____                 _ __       ")
+print("  / ___/______ _____/ /__ ___ ___  / _/ /_  / __/__ ______ ______(_) /___ __ ")
+print(" / /__/ __/ _ `/ __/  '_/(_-</ _ \/ _/ __/ _\ \/ -_) __/ // / __/ / __/ // / ")
+print(" \___/_/  \_,_/\__/_/\_\/___/\___/_/ \__/ /___/\__/\__/\_,_/_/ /_/\__/\_, /  ")
+print("                                                                     /___/  ")
+print(" >> Follow Us on Instagram >>    @its_cracksoft    ,  @cracksoft_security")
+
+
 print("Welcome to CRACKSOFT Page")
 a=input("Enter Your Name \n >>")
 print()
 print("Dear "+a+" Steps to Fix Error \n 1st- Uninstall the Termux \n 2nd- Install the Termux from Playstore \n 3rd- Please Don't install Nethunter Again or \n Cheack your Device in KaliNetHunter, List link is >>bit.ly/3GowBrv")
 print("If your Device is not in list then don't install Nehunter Again , becaure it'll not install and only give you Error \n\n\n\nThanks Dear "+a+" for using CRACKSOFT SECURITY INDIA 1st Tool")
+
+
